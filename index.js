b2c8eb5107967bb597d8bfb07e6b57b8478f8a55
Moritz Sageder <moritzsageder@gmx.at>
2022-06-21 19:44:03 +0200

    Calculate Matrix

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 $(() => {
     const matrix = [];
 
+    let newColRows = 2;
 
     $('#columnRows').on('change', () => {
         $('#matrix').empty();
-        let newColRows = $('#columnRows').val();
+        newColRows = $('#columnRows').val();
         for (let i = 0; i < newColRows; i++) {
             $('#matrix').append(`<tr id="${i}">`)
             for (let j = 0; j < newColRows; j++) {
                 $(`#${i}`).append(`<th><input type="number" id="${i + '' + j}" name="${ i + '' + j}" required value="2"">`);
              }
         }
-    }); 
+    });
+
+    $('#calc').on('click', () => {
+        calc();
+    });
+
+    function calc(){
+        matrix.length = 0;
+        for (let i = 0; i < newColRows; i++) {
+            matrix.push([]);
+            for (let j = 0; j < newColRows; j++) {
+                matrix[i].push($(`#${i}${j}`).val());
+            }
+        }
+        
+        console.log(matrix);
+    }
 });
