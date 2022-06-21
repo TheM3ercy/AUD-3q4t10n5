743beb7fd191b7fe37dc7f68c199d449d47adf51
TheM3ercy <t.k.furjan@gmail.com>
2022-06-21 18:51:33 +0200

    idk mo hat halt voll viel unnötigen code gemacht und das hat nicht wirklich funktioniert deswegen hab ich einfach ein         $('#matrix').empty();         hinzugefügt         und code vom mo gelöscht bis es funktioniert hat         ich bin ein fuckin genius         lol.

--- index.js
+++ index.js
@@ -1,35 +1,15 @@
 $(() => {
     const matrix = [];
-    let columnRow = 2;
 
-    for (let i = 0; i < columnRow; i++) {
-        matrix.push([]);
-        for (let j = 0; j < columnRow; j++) {
-            matrix[i].push($(`#${i}${j}`).val());
-        }
-    }
 
     $('#columnRows').on('change', () => {
+        $('#matrix').empty();
         let newColRows = $('#columnRows').val();
-        if (newColRows >= columnRow) {
-            let diff = newColRows - columnRow;
-            columnRow = newColRows;
-            for (let i = 0; i < diff; i++) {
-                alert(columnRow + i);
-                $('#matrix').append(`<tr id="${Number (columnRow) + i}">`)
-                for (let j = 0; j < columnRow; j++) {
-                    $(`#${Number (columnRow) + i}`).append(`<th><input type="number" id="${columnRow + i + '' + columnRow + j}" name="${columnRow + i + '' + columnRow + j}" required value="2"">`);
-                }
-            }
-        }else{
-            let diff = columnRow - newColRows;
-            for (let i = 0; i < diff; i++) {
-                for (let j = 0; j < columnRow; j++) {
-                $('#matrix').append(`<input type="number" id="${i + '' + j}" name="${i + '' + j}" required value="2"">`);
-                }
-            }
+        for (let i = 0; i < newColRows; i++) {
+            $('#matrix').append(`<tr id="${i}">`)
+            for (let j = 0; j < newColRows; j++) {
+                $(`#${i}`).append(`<th><input type="number" id="${i + '' + j}" name="${ i + '' + j}" required value="2"">`);
+             }
         }
     }); 
-
-    console.log(matrix);
 });
